59461eb2f9a6513bc12512e26e1f030ca809b554
ammusandra13@gmail.com <MY_NAME@example.com>
2023-04-04 08:52:38 +0530

    Updates

---
+++ precache-manifest.68f9982df907680e2be6ad0263bdcb31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805f9044a451aab5d15d0f148ced4a2d",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "940f10b7595760eaa473",
+    "url": "/todo-app/static/css/main.6455930c.chunk.css"
+  },
+  {
+    "revision": "d61ced94dea9da32298a",
+    "url": "/todo-app/static/js/2.f9fd44f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.f9fd44f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940f10b7595760eaa473",
+    "url": "/todo-app/static/js/main.6393f972.chunk.js"
+  },
+  {
+    "revision": "25e665b0e3b42e26ecf7",
+    "url": "/todo-app/static/js/runtime-main.d605437d.js"
+  }
+]);
